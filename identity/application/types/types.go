7a5fffae4ec6540a4e39ede6bfc98923eda689c9
lopesboa <lopes.boa@sanar.com>
2024-04-24 19:22:37 -0300

    feat: add const for context key

--- identity/application/types/types.go
+++ identity/application/types/types.go
@@ -6,6 +6,13 @@ import (
 	"github.com/Nerzal/gocloak/v13"
 )
 
+type contextKey int
+
 type IdentityManager interface {
 	CreateUser(ctx context.Context, user gocloak.User, password string) (*gocloak.User, error)
 }
+
+const (
+	ContextKeyRequestId contextKey = iota
+	ContextKeyClaims    contextKey = iota
+)
