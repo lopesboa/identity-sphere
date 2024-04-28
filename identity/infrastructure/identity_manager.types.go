dc74cf2e0db81d40f6cf91419ca53e6522174f82
lopesboa <lopes.boa@sanar.com>
2024-04-28 12:04:31 -0300

    refactor: adjust types

--- identity/infrastructure/identity_manager.types.go
+++ identity/infrastructure/identity_manager.types.go
@@ -7,9 +7,13 @@ import (
 )
 
 type GoCloakClient interface {
-	LoginClient(ctx context.Context, clientID string, clientSecret string, realm string, scopes ...string) (*gocloak.JWT, error)
-	CreateUser(ctx context.Context, token string, realm string, user gocloak.User) (string, error)
-	SetPassword(ctx context.Context, token string, userID string, realm string, password string, temporary bool) error
-	GetUserByID(ctx context.Context, accessToken string, realm string, userID string) (*gocloak.User, error)
-	GetClientRole(ctx context.Context, token string, realm string, idOfClient string, roleName string) (*gocloak.Role, error)
+	loginClient(ctx context.Context, clientID string, clientSecret string, realm string, scopes ...string) (*gocloak.JWT, error)
+	createUser(ctx context.Context, token string, realm string, user gocloak.User) (string, error)
+	setPassword(ctx context.Context, token string, userID string, realm string, password string, temporary bool) error
+	getUserByID(ctx context.Context, accessToken string, realm string, userID string) (*gocloak.User, error)
+	getClientRole(ctx context.Context, token string, realm string, idOfClient string, roleName string) (*gocloak.Role, error)
+}
+
+type gocloakClient struct {
+	client *gocloak.GoCloak
 }
